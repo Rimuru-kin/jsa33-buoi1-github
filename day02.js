1de6228d5ff94d13a8f11052164bf0c58742563b
Rimuru-kin <119741342+Rimuru-kin@users.noreply.github.com>
2022-12-16 15:47:53 +0700

    Add files via upload

---
+++ day02.js
@@ -0,0 +1,32 @@
+let fruit = [
+    ["Apple", 15],
+    ["Banana", 15],
+    ["Apple", 15]
+]
+console.log('I have ', fruit)
+let text = "";
+let i = 0
+for (let i = 0; i < fruit.length; i++) {
+    text += fruit[i] + "<br>";
+    console.log(fruit);
+}
+while (fruit[i]) {
+    text += fruit[i] + "<br>";
+    i++;
+    console.log(fruit)
+}
+do {
+    text += fruit[i] + i;
+    i++;
+    console.log(fruit)
+}
+while (i < 10);
+let Husky = {
+
+    "name": "Ngáo",
+
+    "legs": 4,
+
+    "color": "Brown"
+}
+console.log("Giới thiệu về chú chó của tôi ", Husky)
